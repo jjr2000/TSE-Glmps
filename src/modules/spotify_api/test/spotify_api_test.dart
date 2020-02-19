ca707eb22658e94da591252bc98341b5442073b5
Jaidon Rymer <jaidon.rymer@gmail.com>
2020-02-19 18:39:59 +0000

    Fixed Test Case

--- src/modules/spotify_api/test/spotify_api_test.dart
+++ src/modules/spotify_api/test/spotify_api_test.dart
@@ -5,6 +5,6 @@ import 'package:spotify_api/spotify_api.dart';
 void main() {
   test('Search for smells like teen spirit', () async {
     final spotifyApi = SpotifyAPI();
-    expect(await spotifyApi.search('smells like teen spirit')[found], SpotifyAlbum());
+    expect((await spotifyApi.search('smells like teen spirit')).found, true);
   });
 }
