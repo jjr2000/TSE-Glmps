3345952719f1e8c6d460e870c9de86f5ce352cd3
Ryan Sweeney <ryan_sweeney@outlook.ie>
2020-02-23 16:35:14 +0000

    This is a push that got pushed in a pushy way

---
+++ src/application/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("Glymps"),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text("Camera stuff goes here")
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text("Button to start scanning")
+    ),
+  ),
+  title: "Glymps"
+));
