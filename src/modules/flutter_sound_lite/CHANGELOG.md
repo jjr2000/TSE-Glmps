73aaa6507cc1e2eb7c56da24c4d1f2a84a58e703
Jaidon Rymer <jaidon.rymer@gmail.com>
2020-03-16 21:52:49 +0000

    Added sound back in

---
+++ src/modules/flutter_sound_lite/CHANGELOG.md
@@ -0,0 +1,111 @@
+## 2.1.1
+- Amazon billing: Trigger purchaseUpdated callback when iap purchased [#165](https://github.com/dooboolab/flutter_inapp_purchase/pull/165).
+## 2.1.0
+- Handle custom audio path from [path_provider](https://pub.dev/packages/path_provider).
+## 2.0.5
+- Hotfix [#221](https://github.com/dooboolab/flutter_sound/issues/221)
+- Use AAC-LC format instead of MPEG-4 [#209](https://github.com/dooboolab/flutter_sound/pull/209)
+## 2.0.4
+- OGG/OPUS support on iOS [#199](https://github.com/dooboolab/flutter_sound/pull/199)
+## 2.0.3
+- Resolve [#194](https://github.com/dooboolab/flutter_sound/issues/194)
+  * `stopReocorder` resolve path.
+- Resolve [#198](https://github.com/dooboolab/flutter_sound/issues/198)
+  * Improve static handler in android.
+
+## 2.0.1
+- Add compatibility for android sdk 19.
+- Add `androidx` compatibility.
+- Resolve [#193](https://github.com/dooboolab/flutter_sound/issues/193)
+  * Restore default `startRecorder`
+
+## 1.9.0
+- Fix issue [#175](https://github.com/dooboolab/flutter_sound/issues/175)
+  - add functions
+      . isEncoderSupported(t_CODEC codec);
+      . isDecoderSupported(t_CODEC codec);
+  - add property 'audioState'
+  - check if codec is really supported before doing 'startRecorder'
+  - modify the example app : disable buttons when the button is not compatible with the current state
+  - in the example, add sound assets encoded with the various encoder
+  - modify the example to play from assets
+  - modify the example to allow selection of various codec
+## 1.7.0
++ startPlayerFromBuffer, to play from a buffer [#170](https://github.com/dooboolab/flutter_sound/pull/170)
+## 1.6.0
++ Set android default encoding option to `AAC`.
++ Fix android default poor sound.
+  - Resolve [#155](https://github.com/dooboolab/flutter_sound/issues/155)
+  - Resolve [#95](https://github.com/dooboolab/flutter_sound/issues/95)
+  - Resolve [#75](https://github.com/dooboolab/flutter_sound/issues/79)
+## 1.5.2
++ Postfix `GetDirectoryType` to avoid conflicts [#147](https://github.com/dooboolab/flutter_sound/pull/147)
+## 1.5.1
++ Set android recorder encoder default value to `AndroidEncoder.DEFAULT`.
+## 1.5.0
++ Use `NSCachesDirectory` instead of `NSTemporaryDirectory` [#141](https://github.com/dooboolab/flutter_sound/pull/141)
+## 1.4.8
++ Resolve [#129](https://github.com/dooboolab/flutter_sound/issues/129)
+## 1.4.7
++ Resolve few issues on `ios` record path.
++ Resolve issue `playing` status so player can resume.
++ Resolve [#134](https://github.com/dooboolab/flutter_sound/issues/134)
++ Resolve [#135](https://github.com/dooboolab/flutter_sound/issues/135)
+## 1.4.4
++ Stopped recording generating infinite db values [#131](https://github.com/dooboolab/flutter_sound/pull/131)
+## 1.4.3
++ Improved db calcs [#123](https://github.com/dooboolab/flutter_sound/pull/123)
+## 1.4.2
++ Fixed 'mediaplayer went away with unhandled events' bug [#104](https://github.com/dooboolab/flutter_sound/pull/104)
+## 1.4.1
++ Fixed 'mediaplayer went away with unhandled events' bug [#83](https://github.com/dooboolab/flutter_sound/pull/83)
+## 1.4.0
++ AndroidX compatibility improved [#68](https://github.com/dooboolab/flutter_sound/pull/68)
++ iOS: Fixes for seekToPlayer [#72](https://github.com/dooboolab/flutter_sound/pull/72)
++ iOS: Setup configuration for using bluetooth microphone recording input [#73](https://github.com/dooboolab/flutter_sound/pull/73)
+
+## 1.3.6
++ Android: Adds a single threaded command scheduler for all recording related
+  commands.
++ Switch source & target compability to Java 8
++ Bump gradle plugin version dependencies
+
+## 1.3.+
++ Support db/meter [#41](https://github.com/dooboolab/flutter_sound/pull/41)
++ Show wrong recorder timer text [#47](https://github.com/dooboolab/flutter_sound/pull/47)
++ Add ability to specify Android & iOS encoder [#49](https://github.com/dooboolab/flutter_sound/pull/49)
++ Adjust db range and fix nullable check in ios [#59](https://github.com/dooboolab/flutter_sound/pull/59)
++ Android: Recording operations on a separate command queue [#66](https://github.com/dooboolab/flutter_sound/pull/66)
++ Android: Remove reference to non-AndroidX classes which improves compatibility
+
+## 1.2.+
+* Fixed sound distorting when playing recorded audio again. Issue [#14](https://github.com/dooboolab/flutter_sound/issues/14).
+* Fixed `seekToPlayer` for android. Issue [#10](https://github.com/dooboolab/flutter_sound/issues/10).
++ Expose recorder `sampleRate` and `numChannel`.
++ Do not append `tmp` when filePath provided in `ios`.
++ Resolve `regression` issue in `1.2.3` which caused in `1.2.2`.
++ Reduce the size of audio file in `1.2.4`. Related [#26](https://github.com/dooboolab/flutter_sound/issues/26).
++ Fixed `recording` issue in android in `1.2.5`.
++ Changed `seekToPlayer` to place exact `secs` instead adding it.
++ Fix file URI for recording and playing in iOS.
+## 1.1.+
+* Released 1.1.0 with beautiful logo from mansa.
+* Improved readme.
+* Resolve #7.
+* Fixed missing break in switch statement.
+## 1.0.9
+* Reimport `intl` which is needed to format date in Dart.
+## 1.0.8
+* Implemented `setVolume` method.
+* Specific error messages given in android.
+* Manage ios player thread when audio is not loaded.
+## 1.0.7
+* Safer handling of progressUpdate in ios when audio is invalid.
+## 1.0.6
+* Fixed bug in platform specific code.
+## 1.0.5
+* Fixed pug in `seekToPlayer` in `ios`.
+## 1.0.3
+* Added license.
+## 1.0.0
+* Released preview version for audio `recorder` and `player`.
---
+++ src/modules/flutter_sound_lite/ios/Classes/FlutterSoundPlugin.m
@@ -0,0 +1,358 @@
+#import "FlutterSoundPlugin.h"
+#import <AVFoundation/AVFoundation.h>
+
+NSString* defaultExtensions [] =
+{
+	  @"sound.aac" 	// CODEC_DEFAULT
+	  @"sound.aac" 	// CODEC_AAC
+	, @"sound.opus"	// CODEC_OPUS
+	, @"sound.caf"	// CODEC_CAF_OPUS
+	, @"sound.mp3"	// CODEC_MP3
+	, @"sound.ogg"	// CODEC_VORBIS
+	, @"sound.wav"	// CODE_PCM
+};
+
+AudioFormatID formats [] =
+{
+	  kAudioFormatMPEG4AAC	// CODEC_DEFAULT
+    , kAudioFormatMPEG4AAC	// CODEC_AAC
+	, 0						// CODEC_OPUS
+	, kAudioFormatOpus		// CODEC_CAF_OPUS
+	, 0						// CODEC_MP3
+	, 0						// CODEC_OGG
+	, 0						// CODEC_PCM
+};
+
+
+bool _isIosEncoderSupported [] =
+{
+    true, // DEFAULT
+    true, // AAC
+    false, // OGG/OPUS
+    true, // CAF/OPUS
+    false, // MP3
+    false, // OGG/VORBIS
+    false, // WAV/PCM
+};
+
+
+bool _isIosDecoderSupported [] =
+{
+    true, // DEFAULT
+    true, // AAC
+    false, // OGG/OPUS
+    true, // CAF/OPUS
+    true, // MP3
+    false, // OGG/VORBIS
+    true, // WAV/PCM
+};
+
+
+// post fix with _FlutterSound to avoid conflicts with common libs including path_provider
+NSString* GetDirectoryOfType_FlutterSound(NSSearchPathDirectory dir) {
+  NSArray* paths = NSSearchPathForDirectoriesInDomains(dir, NSUserDomainMask, YES);
+  return [paths.firstObject stringByAppendingString:@"/"];
+}
+
+@implementation FlutterSoundPlugin{
+  NSURL *audioFileURL;
+  AVAudioRecorder *audioRecorder;
+  AVAudioPlayer *audioPlayer;
+  NSTimer *timer;
+  NSTimer *dbPeakTimer;
+}
+double subscriptionDuration = 0.01;
+double dbPeakInterval = 0.8;
+bool shouldProcessDbLevel = false;
+FlutterMethodChannel* _channel;
+
+- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag {
+  NSLog(@"audioPlayerDidFinishPlaying");
+  NSNumber *duration = [NSNumber numberWithDouble:audioPlayer.duration * 1000];
+  NSNumber *currentTime = [NSNumber numberWithDouble:audioPlayer.currentTime * 1000];
+
+  NSString* status = [NSString stringWithFormat:@"{\"duration\": \"%@\", \"current_position\": \"%@\"}", [duration stringValue], [currentTime stringValue]];
+  /*
+  NSDictionary *status = @{
+                           @"duration" : [duration stringValue],
+                           @"current_position" : [currentTime stringValue],
+                           };
+  */
+  [_channel invokeMethod:@"audioPlayerDidFinishPlaying" arguments:status];
+
+  [self stopTimer];
+}
+
+- (void) stopTimer{
+    if (timer != nil) {
+        [timer invalidate];
+        timer = nil;
+    }
+}
+
+- (void)updateProgress:(NSTimer*) timer
+{
+  NSNumber *duration = [NSNumber numberWithDouble:audioPlayer.duration * 1000];
+  NSNumber *currentTime = [NSNumber numberWithDouble:audioPlayer.currentTime * 1000];
+
+  if ([duration intValue] == 0 && timer != nil) {
+    [self stopTimer];
+    return;
+  }
+
+
+  NSString* status = [NSString stringWithFormat:@"{\"duration\": \"%@\", \"current_position\": \"%@\"}", [duration stringValue], [currentTime stringValue]];
+  /*
+  NSDictionary *status = @{
+                           @"duration" : [duration stringValue],
+                           @"current_position" : [currentTime stringValue],
+                           };
+  */
+
+  [_channel invokeMethod:@"updateProgress" arguments:status];
+}
+
+- (void)updateDbPeakProgress:(NSTimer*) dbPeakTimer
+{
+      NSNumber *normalizedPeakLevel = [NSNumber numberWithDouble:MIN(pow(10.0, [audioRecorder peakPowerForChannel:0] / 20.0) * 160.0, 160.0)];
+      [_channel invokeMethod:@"updateDbPeakProgress" arguments:normalizedPeakLevel];
+}
+
+- (void)startTimer
+{
+  dispatch_async(dispatch_get_main_queue(), ^{
+      self->timer = [NSTimer scheduledTimerWithTimeInterval:subscriptionDuration
+                                           target:self
+                                           selector:@selector(updateProgress:)
+                                           userInfo:nil
+                                           repeats:YES];
+  });
+}
+
+- (void)startDbTimer
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self->dbPeakTimer = [NSTimer scheduledTimerWithTimeInterval:dbPeakInterval
+                                                       target:self
+                                                     selector:@selector(updateDbPeakProgress:)
+                                                     userInfo:nil
+                                                      repeats:YES];
+    });
+}
+
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_sound"
+            binaryMessenger:[registrar messenger]];
+  FlutterSoundPlugin* instance = [[FlutterSoundPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+  _channel = channel;
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"isEncoderSupported" isEqualToString:call.method]) {
+    NSNumber* codec = (NSNumber*)call.arguments[@"codec"];
+    [self isEncoderSupported:[codec intValue] result:result];
+  } else if ([@"isDecoderSupported" isEqualToString:call.method]) {
+     NSNumber* codec = (NSNumber*)call.arguments[@"codec"];
+     [self isDecoderSupported:[codec intValue] result:result];
+  } else if ([@"startPlayer" isEqualToString:call.method]) {
+      NSString* path = (NSString*)call.arguments[@"path"];
+      [self startPlayer:path result:result];
+  } else if ([@"stopPlayer" isEqualToString:call.method]) {
+    [self stopPlayer:result];
+  } else if ([@"pausePlayer" isEqualToString:call.method]) {
+    [self pausePlayer:result];
+  } else if ([@"resumePlayer" isEqualToString:call.method]) {
+    [self resumePlayer:result];
+  } else if ([@"seekToPlayer" isEqualToString:call.method]) {
+    NSNumber* sec = (NSNumber*)call.arguments[@"sec"];
+    [self seekToPlayer:sec result:result];
+  } else if ([@"setSubscriptionDuration" isEqualToString:call.method]) {
+    NSNumber* sec = (NSNumber*)call.arguments[@"sec"];
+    [self setSubscriptionDuration:[sec doubleValue] result:result];
+  } else if ([@"setVolume" isEqualToString:call.method]) {
+    NSNumber* volume = (NSNumber*)call.arguments[@"volume"];
+    [self setVolume:[volume doubleValue] result:result];
+  }
+  else if ([@"setDbPeakLevelUpdate" isEqualToString:call.method]) {
+      NSNumber* intervalInSecs = (NSNumber*)call.arguments[@"intervalInSecs"];
+      [self setDbPeakLevelUpdate:[intervalInSecs doubleValue] result:result];
+  }
+  else if ([@"setDbLevelEnabled" isEqualToString:call.method]) {
+      BOOL enabled = [call.arguments[@"enabled"] boolValue];
+      [self setDbLevelEnabled:enabled result:result];
+  }
+  else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+
+- (void)isDecoderSupported:(t_CODEC)codec result: (FlutterResult)result {
+  NSNumber*  b = [NSNumber numberWithBool: _isIosDecoderSupported[codec] ];
+  result(b);
+}
+
+- (void)isEncoderSupported:(t_CODEC)codec result: (FlutterResult)result {
+  NSNumber*  b = [NSNumber numberWithBool: _isIosEncoderSupported[codec] ];
+  result(b);
+}
+
+- (void)setSubscriptionDuration:(double)duration result: (FlutterResult)result {
+  subscriptionDuration = duration;
+  result(@"setSubscriptionDuration");
+}
+
+- (void)setDbPeakLevelUpdate:(double)intervalInSecs result: (FlutterResult)result {
+    dbPeakInterval = intervalInSecs;
+    result(@"setDbPeakLevelUpdate");
+}
+
+- (void)setDbLevelEnabled:(BOOL)enabled result: (FlutterResult)result {
+    shouldProcessDbLevel = enabled == YES;
+    result(@"setDbLevelEnabled");
+}
+
+- (void)startPlayer:(NSString*)path result: (FlutterResult)result {
+  bool isRemote = false;
+  if ([path class] == [NSNull class]) {
+    audioFileURL = [NSURL fileURLWithPath:[GetDirectoryOfType_FlutterSound(NSCachesDirectory) stringByAppendingString:@"sound.aac"]];
+  } else {
+    NSURL *remoteUrl = [NSURL URLWithString:path];
+    if(remoteUrl && remoteUrl.scheme && remoteUrl.host){
+        audioFileURL = remoteUrl;
+        isRemote = true;
+    } else {
+        audioFileURL = [NSURL URLWithString:path];
+    }
+  }
+
+  if (isRemote) {
+    NSURLSessionDataTask *downloadTask = [[NSURLSession sharedSession]
+        dataTaskWithURL:audioFileURL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            // NSData *data = [NSData dataWithContentsOfURL:audioFileURL];
+            
+        // We must create a new Audio Player instance to be able to play a different Url
+        self->audioPlayer = [[AVAudioPlayer alloc] initWithData:data error:nil];
+        self->audioPlayer.delegate = self;
+
+        // Able to play in silent mode
+        [[AVAudioSession sharedInstance]
+            setCategory: AVAudioSessionCategoryPlayback
+            error: nil];
+        // Able to play in background
+        [[AVAudioSession sharedInstance] setActive: YES error: nil];
+        [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
+
+        [self->audioPlayer play];
+        [self startTimer];
+        NSString *filePath = self->audioFileURL.absoluteString;
+        result(filePath);
+    }];
+
+    [downloadTask resume];
+  } else {
+    // if (!audioPlayer) { // Fix sound distoring when playing recorded audio again.
+      audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:audioFileURL error:nil];
+      audioPlayer.delegate = self;
+    // }
+
+    // Able to play in silent mode
+    [[AVAudioSession sharedInstance]
+        setCategory: AVAudioSessionCategoryPlayback
+        error: nil];
+
+    [audioPlayer play];
+    [self startTimer];
+
+    NSString *filePath = audioFileURL.absoluteString;
+    result(filePath);
+  }
+}
+
+- (void)stopPlayer:(FlutterResult)result {
+  if (audioPlayer) {
+    if (timer != nil) {
+        [timer invalidate];
+        timer = nil;
+    }
+    [audioPlayer stop];
+    audioPlayer = nil;
+    result(@"stop play");
+  } else {
+    result([FlutterError
+        errorWithCode:@"Audio Player"
+        message:@"player is not set"
+        details:nil]);
+  }
+}
+
+- (void)pausePlayer:(FlutterResult)result {
+  if (audioPlayer && [audioPlayer isPlaying]) {
+    [audioPlayer pause];
+    if (timer != nil) {
+        [timer invalidate];
+        timer = nil;
+    }
+    result(@"pause play");
+  } else {
+    result([FlutterError
+        errorWithCode:@"Audio Player"
+        message:@"player is not set"
+        details:nil]);
+  }
+}
+
+- (void)resumePlayer:(FlutterResult)result {
+  if (!audioFileURL) {
+    result([FlutterError
+            errorWithCode:@"Audio Player"
+            message:@"fileURL is not defined"
+            details:nil]);
+    return;
+  }
+
+  if (!audioPlayer) {
+    result([FlutterError
+            errorWithCode:@"Audio Player"
+            message:@"player is not set"
+            details:nil]);
+    return;
+  }
+
+  [[AVAudioSession sharedInstance]
+    setCategory: AVAudioSessionCategoryPlayback
+    error: nil];
+  [audioPlayer play];
+  [self startTimer];
+  NSString *filePath = audioFileURL.absoluteString;
+  result(filePath);
+}
+
+- (void)seekToPlayer:(nonnull NSNumber*) time result: (FlutterResult)result {
+  if (audioPlayer) {
+      audioPlayer.currentTime = [time doubleValue] / 1000;
+      [self updateProgress:nil];
+      result([time stringValue]);
+  } else {
+    result([FlutterError
+        errorWithCode:@"Audio Player"
+        message:@"player is not set"
+        details:nil]);
+  }
+}
+
+- (void)setVolume:(double) volume result: (FlutterResult)result {
+    if (audioPlayer) {
+        [audioPlayer setVolume: volume];
+        result(@"volume set");
+    } else {
+        result([FlutterError
+                errorWithCode:@"Audio Player"
+                message:@"player is not set"
+                details:nil]);
+    }
+}
+
+@end
