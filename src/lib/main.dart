a082777720f8cb9af417e55ddebd343df013e93c
Jaidon Rymer <jaidon.rymer@gmail.com>
2020-04-07 16:23:31 +0100

    Added spotify api to project and fixed git.ignore

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' show join;
 import 'package:path_provider/path_provider.dart';
+import 'package:spotify_api/spotify_api.dart';
 
 //List<CameraDescription> cameras;
 //CameraDescription firstCamera;
