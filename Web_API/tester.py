ebc5336d96b0889ec50c213d858aacba909586f4
Jaidon Rymer <jaidon.rymer@gmail.com>
2020-04-13 18:08:56 +0100

    Fixed scale issue

--- Web_API/tester.py
+++ Web_API/tester.py
@@ -4,7 +4,7 @@ import base64
 import log
 import io
 from PIL import Image
-newsize = (381, 806)
+newsize = (806, 381)
 
 image_file = Image.open("Image_Processor/2.jpg")  
 image_file = image_file.resize(newsize)
