ee9be967651dbd6b1949ee5a50b6fba332d5f968
Matt Atkins <matthewatkins16@gmail.com>
2020-04-18 20:30:08 +0100

    Added album list, still needs data from the spotify list

--- src/lib/pages/library.dart
+++ src/lib/pages/library.dart
@@ -30,7 +30,7 @@ class _LibraryState extends State<Library> {
 
   List<Album> albums = [
     Album(artist: 'Tame Impala', title: 'Lonerism', art:'https://images-na.ssl-images-amazon.com/images/I/81J5ArW6voL._SL1200_.jpg', song: 'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview128/v4/06/73/aa/0673aa28-3b94-6d16-de5f-e3de7d4c8fc3/mzaf_3097933090202335091.plus.aac.p.m4a', songName: 'Feels Like We Only Go Backwards', spotifyLink: 'https://open.spotify.com/album/3C2MFZ2iHotUQOSBzdSvM7'),
-    Album(artist: 'Nouns', title: 'Still Bummed', art:'https://e.snmc.io/i/600/w/a11daacb49cd4a8ddfa6dd8ba830e1e4/7478084', song: 'test3.mp3', songName: 'Dogs', spotifyLink: 'https://open.spotify.com/album/0TmbsFrbOcvrBXLZkVGRru'),
+    Album(artist: 'Nouns', title: 'Still Bummed', art:'https://e.snmc.io/i/600/w/a11daacb49cd4a8ddfa6dd8ba830e1e4/7478084', song: 'https://luan.xyz/files/audio/ambient_c_motion.mp3', songName: 'Dogs', spotifyLink: 'https://open.spotify.com/album/0TmbsFrbOcvrBXLZkVGRru'),
     Album(artist: 'Jack Stauber', title: 'HiLo', art:'https://i.redd.it/58u56d6mkki21.jpg', song: 'test2.mp3', songName: 'Dead Weight', spotifyLink: 'https://open.spotify.com/album/4RsjXMHyCUigESP74GkNHB'),
   ];
 
